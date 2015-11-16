9ba91eb4d21a53794a70c79e78d4ea862f5cb3b5
Richard Neitzke <richardneitzke.rn@gmail.com>
2015-11-16 17:19:43 -0500

    tweaks that make implementing the settings easier later

--- Forecast/SettingsViewController.swift
+++ Forecast/SettingsViewController.swift
@@ -14,6 +14,22 @@ class SettingsViewController: UITableViewController {
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        
+        //Makes Navigation Bar Transparent
+        let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), forBarMetrics: UIBarMetrics.Default)
+        bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        bar.barStyle = .BlackTranslucent
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        let header: UITableViewHeaderFooterView = view as! UITableViewHeaderFooterView //recast your view as a UITableViewHeaderFooterView
+        header.textLabel!.textColor = UIColor.whiteColor() //make the text white
+        header.alpha = 0.5 //make the header transparent
+    }
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         
         if indexPath == NSIndexPath(forItem: 1, inSection: 1)
